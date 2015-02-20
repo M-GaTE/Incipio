354712df39d35537f9c7dec7605415fa924bb54a
Florian Lefevre <florianlefevre@live.fr>
2015-02-20 17:50:56 +0100

    fix(design):ajout fct égalisation height container

--- web/js/mgate.js
+++ web/js/mgate.js
@@ -93,3 +93,10 @@ function showCurrent(){
 		});
 	}
 };
+
+$.fn.equalizeHeights = function() {
+  var maxHeight = this.map(function( i, e ) {
+    return $( e ).height();
+  }).get();
+  return this.height( Math.max.apply( this, maxHeight ) );
+};
